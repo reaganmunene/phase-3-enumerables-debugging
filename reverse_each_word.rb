d44cfbfe421ec5e8ce82353170632c0ef46489f4
reaganmunene <reagannmunene@gmail.com>
2023-07-06 08:44:30 +0300

    completed assignment

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,21 @@
 # Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+
+  words = sentence.split
+  reversed_words= []
+
+  words.each do |word|
+    reversed_words << word.reverse
+
+  end
+  reversed_words.join(" ")
+  # binding.pry
+
+end
+
+binding.pry
+0
+
+# puts reverse_each_word("Hello there, and how are you?")
